e913c7bf2c4ff42fc985b352530c1ccc1dbb9b12
BenedikteEva <cph-bm109@cphbusiness.dk>
2017-11-04 11:33:14 +0100

    Update Order.java
    fjernet confirmation

--- Cupcake/src/main/java/domain/Order.java
+++ Cupcake/src/main/java/domain/Order.java
@@ -19,20 +19,12 @@ public class Order {
      * @param order_id 1
      * @param user_id 2
      * @param reciveddate 3
-     * @param confirmation 4
      */
     private int order_id;
     private String reciveddate;
     private int user_id;
-    private String confirmation;
 
-    public Order(int order_id, int user_id, String reciveddate, String confirmation) {
-        this.order_id = order_id;
-        this.reciveddate = reciveddate;
-        this.user_id = user_id;
-        this.confirmation = confirmation;
-    }
-
+   
     public Order(int user_id) {
         this.user_id = user_id;
     }
@@ -47,15 +39,9 @@ public class Order {
 
     @Override
     public String toString() {
-        return "Order{" + "order_id=" + order_id + ", reciveddate=" + reciveddate + ", user_id=" + user_id + ", confirmation=" + confirmation + '}';
+        return "Order{" + "order_id=" + order_id + ", reciveddate=" + reciveddate + ", user_id=" + user_id + '}';
     }
 
-    public Order(int order_id, String reciveddate, int user_id, String confirmation) {
-        this.order_id = order_id;
-        this.reciveddate = reciveddate;
-        this.user_id = user_id;
-        this.confirmation = confirmation;
-    }
 
     public Order(int order_id, String reciveddate) {
         this.order_id = order_id;
@@ -68,14 +54,6 @@ public class Order {
 
     public void setUser_id(int user_id) {
         this.user_id = user_id;
-    }
-
-    public String getConfirmation() {
-        return confirmation;
-    }
-
-    public void setConfirmation(String confirmation) {
-        this.confirmation = confirmation;
     }
 
     public int getOrder_id() {
